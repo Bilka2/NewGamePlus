f80e85e769cf94ebccad6d2e912f2174279003ba
Bilka <Bilka2@users.noreply.github.com>
2018-01-27 15:20:24 +0100

    Also fix settings.lua indentation

--- settings.lua
+++ settings.lua
@@ -1,8 +1,8 @@
 data:extend({
-   {
-      type = "bool-setting",
-      name = "new-game-plus-allow-early-gui",
-      setting_type = "runtime-global",
-      default_value = false
-   }
+  {
+    type = "bool-setting",
+    name = "new-game-plus-allow-early-gui",
+    setting_type = "runtime-global",
+    default_value = false
+  }
 })
