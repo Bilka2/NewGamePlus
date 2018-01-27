2dc8155045e611953743c4cbe67dcc8b818e2588
Bilka <Bilka2@users.noreply.github.com>
2018-01-27 11:27:30 +0100

    Fixed that the player didnt consider tile transitions

--- data.lua
+++ data.lua
@@ -1,1 +1,1 @@
-data.raw.player.player.collision_mask = { "player-layer", "train-layer", "not-colliding-with-itself"}
+data.raw.player.player.collision_mask = { "player-layer", "train-layer", "not-colliding-with-itself", "consider-tile-transitions"}
